e1248faeb5a913ab4667fe5014309b416ff7e5a4
Yassine Afi <yassineafi31@gmail.com>
2026-05-19 01:19:30 +0100

    Add protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home      from './pages/Home';
-import Login     from './pages/admin/Login';
-import Dashboard from './pages/admin/Dashboard';
+import Home        from './pages/Home';
+import Login       from './pages/admin/Login';
+import Dashboard   from './pages/admin/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
   return (
@@ -9,7 +10,11 @@ export default function App() {
       <Routes>
         <Route path="/"            element={<Home />} />
         <Route path="/admin/login" element={<Login />} />
-        <Route path="/admin"       element={<Dashboard />} />
+        <Route path="/admin"       element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
       </Routes>
     </BrowserRouter>
   );
